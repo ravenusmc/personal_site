1b31cf442ea355fc21c6e578bd59a15175e441cb
ravenusmc <mcuddy77@gmail.com>
2020-04-19 10:05:18 -0400

    started work on landing page

---
+++ index.php
@@ -0,0 +1,15 @@
+<!-- links for the web page -->
+<link rel="stylesheet" type="text/css" href="./assets/css/landing.css">
+<link rel="stylesheet" type="text/css" href="./assets/css/landing.css">
+
+<!-- Main portion of the web page -->
+<main role="main">
+
+  <section>
+    <div class='headerDiv'>
+      <h1 class='font'>Home Data</h1>
+    </div>
+  </section>
+
+</main>
+<!-- End of Main portion -->
