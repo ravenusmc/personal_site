b8a1c24bbade5659662d1f0af201147138ea00d6
ravenusmc <mcuddy77@gmail.com>
2020-04-20 16:20:01 -0400

    redesigned home page

--- index.php
+++ index.php
@@ -5,21 +5,10 @@
 <!-- Main portion of the web page -->
 <main role="main">
 
-  <section>
-
-    <div class='topSection'>
-      <h1 class='fontColor'>Mike Cuddy</h1>
-    </div>
-
-    <div class='centerPiece'>
-      <h1 class='fontColor'>Center</h1>
-    </div>
-
-    <div class='bottomSection'>
-      <h1 class='fontColor'>Web Artisan</h1>
-    </div>
-
-  </section>
+  <div class='headDiv'>
+    <h1>Mike Cuddy</h1>
+    <h2>My Coding Journey</h2>
+  </div>
 
 </main>
 <!-- End of Main portion -->
