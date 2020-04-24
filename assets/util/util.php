9e08bf59c66af2cd74cd675a1a5dd76cf236092a
ravenusmc <mcuddy77@gmail.com>
2020-04-24 16:29:25 -0400

    working on early years page

---
+++ assets/util/util.php
@@ -0,0 +1,6 @@
+<?php
+  $docRoot = "http://localhost/";
+  $appPath = "personal_site/";
+  $fullPath = $docRoot . $appPath;
+  set_include_path($fullPath);
+?>
--- controller/early_years.php
+++ controller/early_years.php
@@ -1,6 +1,20 @@
 <?php
 
 ?>
+<!-- Links for CSS -->
+<link rel="stylesheet" type="text/css" href="./assets/css/early_years.css">
+<link rel="stylesheet" type="text/css" href="./assets/css/generic.css">
+
 <?php include '../view/header.php'; ?>
-<h1>Early Years</h1>
+
+<div >
+  <h1 class='center'>The Early Years</h1>
+</div>
+
+<!-- Main Area -->
+<main role="main">
+
+</main>
+<!-- End of Main Area -->
+
 <?php include '../view/footer.php'; ?>
