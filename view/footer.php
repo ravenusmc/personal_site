6ec649cf394595c82ba031f7cb3d0698b1c2cde6
ravenusmc <mcuddy77@gmail.com>
2020-04-30 15:46:59 -0400

    added to footer

--- view/footer.php
+++ view/footer.php
@@ -1,9 +1,22 @@
-<footer>
-    <p class="font copyright">
-        &copy; <?php echo date("Y"); ?> Double Sun Productions
-    </p>
-</footer>
+<!-- Links for CSS -->
+<link rel="stylesheet" type="text/css" href="../assets/css/footer.css">
+<link rel="stylesheet" type="text/css" href="../assets/css/generic.css">
 
+<!-- Footer Area -->
+<footer>
+  <div class='footer_fix'>
+    <h2 class='footerTitle font'>&copy; <?php echo date("Y"); ?> Double Sun Productions<br></h2>
+    <div>
+      <a href="https://www.linkedin.com/in/mcuddy1/"><i class="fa fa-linkedin-square fa-3x" aria-hidden="true"></i></a>
+      <a href="https://twitter.com/mcuddy7"><i class="fa fa-twitter-square fa-3x" aria-hidden="true"></i></a>
+      <a href="https://medium.com/@mcuddy7"><i class="fa fa-pencil fa-3x" aria-hidden="true"></i></a>
+      <a href="https://github.com/ravenusmc"><i class="fa fa-github-square fa-3x" aria-hidden="true"></i></a>
+    </div>
+  </div>
+</footer>
+<!-- End of Footer Area -->
+
+<!-- BootStrap Links -->
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
