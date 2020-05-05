cf53bdd8493f839cd381ed86fa33e10d101614a3
ravenusmc <mcuddy77@gmail.com>
2020-05-05 11:58:44 -0400

    added photoshop area to current page

--- controller/GTC.php
+++ controller/GTC.php
@@ -74,31 +74,30 @@
 <!-- End of Pic Area -->
 
 <!-- Past Course Area -->
-<div>
-  <h1 id='area_two' class='center'>Past Courses</h1>
-  <div class='courses_area'>
+<div class='photoshopArea'>
+  <h1 id='area_two' class='center'>PhotoShop Work</h1>
+  <div class='photoShopDiv'>
 
-    <div class='fix_list_info'>
-      <h2>Courses with work online:</h2>
-      <ul>
-        <li><a class='change_link' href="1510/index.html">CIST 1510 - Web Development 1</a></li><br>
-        <li><a class='change_link' href="1520/index.html">CIST 1520 - Scripting Technologies</a></li><br>
-        <li><a class='change_link' href="1530/index.html">CIST 1530 - Web Graphics 1</a></li>
-      </ul>
+    <div class='photoShopParagraph'>
+      <h2 class='center'>PhotoShop...Really?</h2>
+      <p>
+        Gwinnett Technical College mandated that every web developer had to take
+        not one but two photoshop classes. When I learned this, it really bothered
+        me. I had no intention of learning photoshop. I wanted more classes to
+        program! Then, about halfway or so through my first photoshop class
+        something started to happen. I found that I actually liked it! When I
+        started my second photoshop class I decided that I'd start building a site
+        for my personal photoshop work. From time to time, I open up photoshop
+        and do some work! So take a look at my work...
+      </p>
     </div>
 
     <div class='fix_list_info'>
-      <h2>Courses with non-online work:</h2>
-      <ul>
-        <li class='non_online_font_size_fix'>CIST 1001 - Computer Concepts</li>
-        <li class='non_online_font_size_fix'>CIST 1305 - Program Design and Development</li>
-        <li class='non_online_font_size_fix'>CIST 1601 - Information Security Fundamentals</li>
-        <li class='non_online_font_size_fix'>CIST 2742 - Beginning Python Programming</li>
-        <li class='non_online_font_size_fix'>CIST 2361 - C++ Programming 1</li>
-        <li class='non_online_font_size_fix'>CIST 2740 - Intro to Game Development</li>
-      </ul>
+      <a href="../photoshop/photoshop_personal_work/pages/index.html"><img src="../assets/images/photoShop.png" alt="photoshop icon"></a>
     </div>
+
   </div>
+
 </div>
 <!-- End of Past Course Area -->
 
