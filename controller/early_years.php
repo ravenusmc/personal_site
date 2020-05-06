bfe35ed6fc8a0731fd1bc387cd686a03e8a3687e
ravenusmc <mcuddy77@gmail.com>
2020-05-06 16:25:03 -0400

    added more to early years page

--- controller/early_years.php
+++ controller/early_years.php
@@ -20,11 +20,32 @@
     <div>
       <p>
         My first experience with programing, where I was drawn in, may have been
-        playing Super Mario Bros, on the NES, when I was in first grade.
+        playing Super Mario Bros, on the NES, when I was in first grade. As I moved
+        Mario slowly across the screen in world 1-1, my mind was transported to
+        a digital realm. I wanted to understand how that digital realm functioned.
+        How did the code let me jump only so high? Why could I not move the screen
+        to the left? How could blocks appear to float in the sky in defiance of
+        gravity. At one point during elementary school, in the early 90's, I remember
+        thinking that I wanted to be a video game programmer. I wanted to make
+        those virtual worlds. It would be years though before I'd ever start
+        programming. That did not happen until high school. It was not a class that
+        I took. I just wanted to learn - the language that I choose...well I don't
+        remember. I want to say it was Basic but I just have no idea. My brother
+        had a friend who was a programmer and he may have told me to choose it. I
+        had a book that guided you how to learn the language and I spent many Friday
+        nights slowly learning the language. I focused on making this choose your
+        own adventure game - at one point the screen turned red if you choose a
+        specific option. Not the coolest. However, I remember I let my little
+        brother try the game out. He and I always fought and I think he thought
+        the game was dumb at first. As he started to play it and saw that the screen
+        would change color based on his actions I think he bacame a little
+        impressed. For me, I had a basic version of that digital world that I so
+        wanted to learn how to do back in First Grade...
       </p>
     </div>
 
     <div>
+      <img class='marioImage' src="../assets/images/mario.png" alt="Mario Image">
     </div>
 
   </section>
