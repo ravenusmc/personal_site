8688744f85f5787594ffc5d90fed6bf3696403bc
ravenusmc <mcuddy77@gmail.com>
2020-05-06 15:41:01 -0400

    started work on early years page

--- controller/early_years.php
+++ controller/early_years.php
@@ -2,18 +2,32 @@
 
 ?>
 <!-- Links for CSS -->
-<link rel="stylesheet" type="text/css" href="./assets/css/early_years.css">
-<link rel="stylesheet" type="text/css" href="./assets/css/generic.css">
+<link rel="stylesheet" type="text/css" href="../assets/css/early_years.css">
+<link rel="stylesheet" type="text/css" href="../assets/css/generic.css">
 
 <!-- Header area -->
 <?php include '../view/header.php'; ?>
 
-<div >
+<div class='headerArea'>
   <h1 class='center'>The Early Years</h1>
 </div>
 
 <!-- Main Area -->
 <main role="main">
+
+  <section id='firstSection'>
+
+    <div>
+      <p>
+        My first experience with programing, where I was drawn in, may have been
+        playing Super Mario Bros, on the NES, when I was in first grade.
+      </p>
+    </div>
+
+    <div>
+    </div>
+
+  </section>
 
 </main>
 <!-- End of Main Area -->
