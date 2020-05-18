77b2bd2efcd066ba6e16bd65617f9f84c6c80f12
ravenusmc <mcuddy77@gmail.com>
2020-05-18 09:09:00 -0400

    made changes to the landing page

--- index.php
+++ index.php
@@ -1,9 +1,28 @@
-<!-- links for the web page -->
-<link rel="stylesheet" type="text/css" href="./assets/css/landing.css">
-<link rel="stylesheet" type="text/css" href="./assets/css/generic.css">
+<!DOCTYPE html>
+<html lang="en">
+<html>
+<!-- the head section -->
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <title name="description" content="Mike Cuddy's Web Page And Web Projects.">Mike Cuddy's Page</title>
+  <!-- Bootstrap CDN -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <!-- Font Awesome -->
+  <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+  <!-- CSS Style Sheets -->
+  <link rel="stylesheet" type="text/css" href="../assets/css/generic.css">
+  <!-- Google Font -->
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@1,300&display=swap" rel="stylesheet">
+  <!-- links for the web page -->
+  <link rel="stylesheet" type="text/css" href="./assets/css/landing.css">
+  <link rel="stylesheet" type="text/css" href="./assets/css/generic.css">
+</head>
+<body>
 
-<!-- Main portion of the web page -->
-<main role="main">
+<!-- Main portion of the web page for desktop-->
+<main id='desktopMain' role="main">
 
   <div class='headDiv font'>
     <h1>Mike Cuddy</h1>
@@ -13,4 +32,18 @@
   </div>
 
 </main>
-<!-- End of Main portion -->
+<!-- End of Main portion for desktop -->
+
+<!-- Main portion of the web page for mobile -->
+<main id='mobileMain' role="main">
+
+  <div class='headDiv font'>
+    <h1>A Coding Journey</h1>
+    <button><a class='link' href='controller'>Enter</a></button>
+  </div>
+
+</main>
+<!-- End of Main portion for mobile-->
+
+</body>
+</html>
