967433e3cfa322cb4ded157d4dccdd5d7117f052
ravenusmc <mcuddy77@gmail.com>
2020-05-19 09:17:33 -0400

    working on home page for personal site

--- controller/home.php
+++ controller/home.php
@@ -1,13 +1,30 @@
-<?php
-?>
-<!-- links for the web page -->
-<link rel="stylesheet" type="text/css" href="../assets/css/home.css">
-<link rel="stylesheet" type="text/css" href="../assets/css/generic.css">
-
+<!DOCTYPE html>
+<html lang="en">
+<html>
+<!-- the head section -->
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <title name="description" content="Mike Cuddy's Web Page And Web Projects.">Mike Cuddy's Page</title>
+  <!-- Bootstrap CDN -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <!-- Font Awesome -->
+  <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+  <!-- CSS Style Sheets -->
+  <link rel="stylesheet" type="text/css" href="../assets/css/generic.css">
+  <!-- Google Font -->
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@1,300&display=swap" rel="stylesheet">
+  <!-- links for the web page -->
+  <link rel="stylesheet" type="text/css" href="../assets/css/home.css">
+  <link rel="stylesheet" type="text/css" href="../assets/css/generic.css">
+</head>
+<body>
+  
 <!-- Start of main area -->
-<main role="main">
+<main id='desktopMain' role="main">
 
-  <div class='stagesDiv'>
+  <div class='stagesDivDesktop'>
     <h1 class='title font'>My Journey<h1>
     <br>
     <p><a class='link font' href="?action=early_years">Early Years / Starting Out</a></p>
@@ -18,5 +35,14 @@
     <br>
   </div>
 
+  <div class='stagesDivMobile'>
+    <button><a class='link font' href="?action=early_years">Early Years / Starting Out</a></button>
+    <button><a class='link font' href="?action=GTC">Getting Serious</a></button>
+    <button><a class='link font' href="?action=current">Current</a></button>
+  </div>
+
 </main>
 <!-- End of Main Area -->
+
+</body>
+</html>
